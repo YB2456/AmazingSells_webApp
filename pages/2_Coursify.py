c2a9c1e4d60575cd33d6083a7b10d668b75d7783
YB2456 <131649411+YB2456@users.noreply.github.com>
2024-06-09 13:07:05 +0400

    Update 2_Coursify.py

--- pages/2_Coursify.py
+++ pages/2_Coursify.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 
 
-logo=Image.open("C://Users//hp//OneDrive//My files//Python Web//AmazingSells//logo.png")
+logo=Image.open("logo.png")
 
 st.set_page_config(page_title="Coursify · AmazingSells", page_icon=logo, layout='wide')
 
